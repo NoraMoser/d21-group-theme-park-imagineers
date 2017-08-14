6cc8e17dce4a5584d4459979853f9d05209869e7
Ted Pennington <tedpennington@Teds-MacBook-Pro.local>
2017-08-14 18:44:18 -0500

    Grid created and displays data loaded from firebase, background colors change based on the color data.

--- javascripts/main.js
+++ javascripts/main.js
@@ -12,3 +12,32 @@ imaginationFactory.loadData()
     (reject) => {
         console.log("something went wrong");
     });
+
+
+
+// Ted: Calling loadAreas function to get the areas data.  Promise returns and then calls populateAreas.
+imaginationFactory.loadAreas()
+.then(
+    (dataFromFactory) => {
+        console.log("areas promise", dataFromFactory);
+        populateAreas(dataFromFactory);
+    },
+    (reject) => {
+        console.log("something went wrong");
+    });
+
+// Takes the data loaded above and populates the grid with names/descriptions, and changes background color based on colorTheme.
+function populateAreas(areasData) {
+	let gridBoxes = $(".grid-box-data");
+	// console.log("grid boxes", gridBoxes);
+	for (let i = 0; i < areasData.length; i++) {
+		// console.log("each area", areasData[i]);
+		// console.log("the stuff", $(".grid-box").html());
+		gridBoxes[i].innerHTML =`
+		 					<h3>${areasData[i].name}</h3>
+							<p>${areasData[i].description}</p>`;
+		gridBoxes[i].setAttribute('style', `background-color: #${areasData[i].colorTheme}`);
+		console.log("descriptions", areasData[i].description);
+
+	}
+}
