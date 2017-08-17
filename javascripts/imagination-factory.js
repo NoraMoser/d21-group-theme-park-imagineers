307a8b2f99759d600bce406cd8adf5fc89df4fab
palmierie <ericmpalmieri@gmail.com>
2017-08-17 15:29:22 -0500

    cleaned toilets

--- javascripts/imagination-factory.js
+++ javascripts/imagination-factory.js
@@ -4,64 +4,33 @@ console.log ("imagination factory");
 
 let Attractory = {};
 
-Attractory.loadData = () => {
-	return new Promise ( function (resolve, reject) {
-		let dataLoader = new XMLHttpRequest();
-		dataLoader.open("GET", "https://imagine-nation-data.firebaseio.com/.json");
-		dataLoader.send();
-
-		dataLoader.addEventListener("load", function () {
-			let data = JSON.parse(this.responseText);
-			// console.log ("data from json", data);
-			resolve(data);
-		});
-	});
-};
+let areasLoaded = [];
+let typesLoaded = [];
 
 // Ted: Load the areas data from firebase
-let areasLoaded = [];
+Attractory.getAllTypes = () => {
+	return typesLoaded;
+};
+
+
+Attractory.getAreaArray = () => {
+	return areasLoaded;
+};
+
 
 Attractory.loadAreas = () => {
 	return new Promise ( function (resolve, reject) {
 		let dataLoader = new XMLHttpRequest();
 		dataLoader.open("GET", "https://imagine-nation-data.firebaseio.com/areas.json");
 		dataLoader.send();
-
 		dataLoader.addEventListener("load", function () {
 			let data = JSON.parse(this.responseText);
 			areasLoaded = data;
-			console.log ("areasLoaded", areasLoaded); 
-			// console.log ("areas data on load", data);
 			resolve(data);
 		});
 	});
 };
 
-Attractory.getAreaArray = () => {
-	return areasLoaded;
-};
-
-let typesLoaded = [];
-
-Attractory.loadTypes = () => {
-	return new Promise ( function (resolve, reject) {
-		let dataLoader = new XMLHttpRequest();
-		dataLoader.open("GET", "https://imagine-nation-data.firebaseio.com/attraction_types.json");
-		dataLoader.send();
-
-		dataLoader.addEventListener("load", function () {
-			let data = JSON.parse(this.responseText);
-			typesLoaded = data;
-			// console.log ("areas data on load", data);
-			resolve(data);
-		});
-	});
-};
-
-Attractory.getAllTypes = () => {
-	return typesLoaded;
-};
- 
 
 //Tyler: Load the specific attractions from firebase
 Attractory.loadAttractions = () => {
@@ -69,10 +38,22 @@ Attractory.loadAttractions = () => {
 		let attractionDataLoader = new XMLHttpRequest();
 		attractionDataLoader.open("GET", "https://imagine-nation-data.firebaseio.com/attractions.json");
 		attractionDataLoader.send();
-
 		attractionDataLoader.addEventListener("load", function () {
 			let data = JSON.parse(this.responseText);
-			// console.log ("attractions data on load", data);
+			resolve(data);
+		});
+	});
+};
+
+
+Attractory.loadTypes = () => {
+	return new Promise ( function (resolve, reject) {
+		let dataLoader = new XMLHttpRequest();
+		dataLoader.open("GET", "https://imagine-nation-data.firebaseio.com/attraction_types.json");
+		dataLoader.send();
+		dataLoader.addEventListener("load", function () {
+			let data = JSON.parse(this.responseText);
+			typesLoaded = data;
 			resolve(data);
 		});
 	});
--- javascripts/main.js
+++ javascripts/main.js
@@ -13,44 +13,6 @@ let Time = require("../javascripts/timemanip.js");
 var attractionsWithTimes = [];
 var attractionsPushedToDOM = [];
 
-imaginationFactory.loadData()
-.then(
-    (dataFromFactory) => {
-        // console.log("factory promise", dataFromFactory);
-    },
-    (reject) => {
-        console.log("something went wrong");
-    });
-
-
-
-// Ted: Calling loadAreas function to get the areas data.  Promise returns and then calls populateAreas.
-imaginationFactory.loadAreas()
-.then(
-    (dataFromFactory) => {
-        // console.log("areas promise", dataFromFactory);
-        populateAreas(dataFromFactory);
-    },
-    (reject) => {
-        console.log("something went wrong");
-    });
-
-
-// Takes the data loaded above and populates the grid with names/descriptions, and changes background color based on colorTheme.
-function populateAreas(areasData) {
-	let gridBoxes = $(".grid-box-data");
-	// console.log("grid boxes", gridBoxes);
-	for (let i = 0; i < areasData.length; i++) {
-		// console.log("each area", areasData[i]);
-		// console.log("the stuff", $(".grid-box").html());
-		gridBoxes[i].innerHTML =`
-		 					<h3>${areasData[i].name}</h3>
-							<p>${areasData[i].description}</p>`;
-		gridBoxes[i].setAttribute('style', `background-color: #${areasData[i].colorTheme}`);
-		// console.log("descriptions", areasData[i].description);
-
-	}
-}
 
 // Current Date
 var today = new Date(); 
@@ -59,12 +21,47 @@ var output = document.getElementById("currentTime");
 
 output.innerHTML = date;
 
-//TIME DROP DOWN
 
+// Takes the data loaded above and populates the grid with names/descriptions, and changes background color based on colorTheme.
+function populateAreas(areasData) {
+	let gridBoxes = $(".grid-box-data");
+	for (let i = 0; i < areasData.length; i++) {
+		gridBoxes[i].innerHTML =`
+		 					<h3>${areasData[i].name}</h3>
+							<p>${areasData[i].description}</p>`;
+		gridBoxes[i].setAttribute('style', `background-color: #${areasData[i].colorTheme}`);
+	}
+}
+
+function populatePage(factoryData) {
+    let newDiv = document.createElement("div");
+    newDiv.innerHTML = attractionTemplate(factoryData);
+    $("#attraction-column").append(newDiv);
+    $(".card").click(function(e){
+        $(".seent").addClass("hidden");
+        $(e.currentTarget).find(".seent").removeClass("hidden");
+    });
+}
+
+
+// Ted: Calling loadAreas function to get the areas data.  Promise returns and then calls populateAreas.
+imaginationFactory.loadAreas()
+.then(
+    (dataFromFactory) => {
+        populateAreas(dataFromFactory);
+    },
+    (reject) => {
+        console.log("something went wrong");
+    });
+
+
+//TIME DROP DOWN
 $('.dropdown-radio').find('input').change(function() {
-  var timeDropdown = $('.dropdown');
-  var radioname = $(this).attr('name');
-  var checked = 'input[name=' + radioname + ']:checked';
+    var mapGridChildren = $(".grid-box-data");
+    mapGridChildren.removeClass("selected-border");
+    var timeDropdown = $('.dropdown');
+    var radioname = $(this).attr('name');
+    var checked = 'input[name=' + radioname + ']:checked';
   
   //update the text
   var checkedText = $(checked).closest('.dropdown-radio').text();
@@ -72,9 +69,7 @@ $('.dropdown-radio').find('input').change(function() {
 
   //retrieve the checked value 
   var timeValue = timeDropdown.find( checked ).val();
-  console.log('timeValue', timeValue);
   var selectedTime = moment(timeValue, "hh:mm a");
-  console.log('selectedTime', selectedTime);
   selectedTimeUmCrapTimesOrSumthin(selectedTime);
   
 });
@@ -84,27 +79,19 @@ function selectedTimeUmCrapTimesOrSumthin(time){
     attractionsPushedToDOM = [];
     $("#attraction-column").html('');
     for (let k = 0; k < attractionsWithTimes.length; k++) {
-                for( let g = 0; g < attractionsWithTimes[k].times.length; g++ ){
-                   // console.log('attractionsWithTimes[k].times[g]', attractionsWithTimes[k].name, attractionsWithTimes[k].times[g]);
-                    if(moment(attractionsWithTimes[k].times[g], "hh:mm a").isBetween(moment(time),moment(time).add(1,"h")) === true){
-                        attractionsPushedToDOM.push(attractionsWithTimes[k]);
-                        break;
-                    }
+        for( let g = 0; g < attractionsWithTimes[k].times.length; g++ ){
+            if(moment(attractionsWithTimes[k].times[g], "hh:mm a").isBetween(moment(time),moment(time).add(1,"h")) === true){
+                attractionsPushedToDOM.push(attractionsWithTimes[k]);
+                break;
             }
+        }
     } 
-    populatePage(attractionsPushedToDOM);
-    console.log('attractionsPushedToDOM', attractionsPushedToDOM);
-    
-    console.log('something is happening');
-    
+    populatePage(attractionsPushedToDOM);   
 }
 //Return ID on area clicked and populate Attraction list
 $('#map-grid').click(function(event){
     var mapGridChildren = $(".grid-box-data");
     mapGridChildren.removeClass("selected-border");
-    
-    console.log('mapGridChildren', mapGridChildren);
-
     var areaClicked = event.target.closest('.grid-box-data').getAttribute('id').slice(6);
     event.target.closest('.grid-box-data').classList.add("selected-border");
     imaginationFactory.loadAttractions()
@@ -112,14 +99,12 @@ $('#map-grid').click(function(event){
             (dataFromFactory) => {
                 attractionsPushedToDOM = [];
                 $("#attraction-column").html('');
-                // console.log("attractions", dataFromFactory);
                 for (let i = 0; i < dataFromFactory.length; i++) {
                     if(dataFromFactory[i].area_id == areaClicked){
                         attractionsPushedToDOM.push(dataFromFactory[i]);
                     }
                 }     
                 populatePage(attractionsPushedToDOM);
-
             },
             (reject) => {
                 console.log("something went wrong");
@@ -128,34 +113,14 @@ $('#map-grid').click(function(event){
 });
 
 
-
-
-function populatePage(factoryData) {
-    let newDiv = document.createElement("div");
-    newDiv.innerHTML = attractionTemplate(factoryData);
-    $("#attraction-column").append(newDiv);
-    console.log('factoryData', factoryData);
-
-    $(".card").click(function(e){
-    console.log("seent");
-    $(".seent").addClass("hidden");
-    $(e.currentTarget).find(".seent").removeClass("hidden");
-});
-    
-}
-
 //This is calling the loadAttractions to get data for all 131 attractions
 imaginationFactory.loadAttractions()
 .then(
     (dataFromFactory) => {
         Search.storeData(dataFromFactory);        
-        
-        
-
-
+    
         attractionsPushedToDOM = [];
         $("#attraction-column").html('');
-        // console.log("attractions", dataFromFactory);
         for (let i = 0; i < dataFromFactory.length; i++) {
             if(dataFromFactory[i].times){
                 attractionsWithTimes.push(dataFromFactory[i]);
@@ -170,7 +135,6 @@ imaginationFactory.loadAttractions()
            }
         }     
         populatePage(attractionsPushedToDOM);
-
     },
     (reject) => {
         console.log("something went wrong");
@@ -178,30 +142,13 @@ imaginationFactory.loadAttractions()
 
 //This is the search button using fuse module.
 $("#search-input").keypress(function(e){
-         $("#attraction-column").html('');
-       let result = Search.searchFunction($(this).val());
-        populatePage(result);
-        if (e.which == 13) {
-            $(this).blur();
-        } 
-    });
-
-
+    $("#attraction-column").html('');
+    let result = Search.searchFunction($(this).val());
+    populatePage(result);
+    if (e.which == 13) {
+        $(this).blur();
+    } 
+});
 $("#search-input").focus(function(e){
     $(this).val("");
 });
-
-
-// $("#search-input").keyup(function(e) {
-    
-// })
-
-
-
-
-    
-
-
-
-
-
